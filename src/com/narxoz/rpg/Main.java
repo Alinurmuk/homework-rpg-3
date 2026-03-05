e181aaefce463adedf865318c2864e7ce9908875
Alinurmuk <alimur.mukazhan@narxoz.kz>
2026-03-05 11:46:40 +0500

    main

--- src/com/narxoz/rpg/Main.java
+++ src/com/narxoz/rpg/Main.java
@@ -1,50 +1,27 @@
 package com.narxoz.rpg;
 
-import com.narxoz.rpg.adapter.EnemyCombatantAdapter;
-import com.narxoz.rpg.adapter.HeroCombatantAdapter;
-import com.narxoz.rpg.battle.BattleEngine;
-import com.narxoz.rpg.battle.Combatant;
-import com.narxoz.rpg.battle.EncounterResult;
-import com.narxoz.rpg.enemy.Goblin;
-import com.narxoz.rpg.hero.Mage;
-import com.narxoz.rpg.hero.Warrior;
-
+import com.narxoz.rpg.battle.*;
+import com.narxoz.rpg.adapter.*;
+import com.narxoz.rpg.hero.*;
+import com.narxoz.rpg.enemy.*;
 import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("=== RPG Battle Engine Demo ===\n");
+        BattleEngine engine = BattleEngine.getInstance();
 
-        // TODO: Create heroes and enemies
-        Warrior warrior = new Warrior("Arthas");
-        Mage mage = new Mage("Jaina");
-        Goblin goblin = new Goblin();
-
-        // TODO: Wrap with adapters
         List<Combatant> heroes = new ArrayList<>();
-        heroes.add(new HeroCombatantAdapter(warrior));
-        heroes.add(new HeroCombatantAdapter(mage));
+        heroes.add(new HeroCombatantAdapter(new Warrior("Arthur")));
+        heroes.add(new HeroCombatantAdapter(new Mage("Gandalf")));
 
         List<Combatant> enemies = new ArrayList<>();
-        enemies.add(new EnemyCombatantAdapter(goblin));
+        enemies.add(new EnemyCombatantAdapter(new Goblin()));
+        enemies.add(new EnemyCombatantAdapter(new BasicEnemy("Orc", 15, 60)));
 
-        // TODO: Demonstrate Singleton behavior
-        BattleEngine engineA = BattleEngine.getInstance();
-        BattleEngine engineB = BattleEngine.getInstance();
-        System.out.println("Same instance? " + (engineA == engineB));
-        System.out.println();
+        EncounterResult result = engine.runEncounter(heroes, enemies);
 
-        // TODO: Run battle and print summary
-        engineA.setRandomSeed(42L);
-        EncounterResult result = engineA.runEncounter(heroes, enemies);
-
-        System.out.println("Winner: " + result.getWinner());
-        System.out.println("Rounds: " + result.getRounds());
-        for (String line : result.getBattleLog()) {
-            System.out.println(line);
-        }
-
-        System.out.println("\n=== Demo Complete ===");
+        result.getBattleLog().forEach(System.out::println);
+        System.out.println("Total Rounds: " + result.getRounds());
     }
 }
